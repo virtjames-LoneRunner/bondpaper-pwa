e6a5753b09c6903dc4723922fde9a66b27521dd5
James Virtudazo <virtjameslourd55@gmail.com>
2025-03-17 20:48:17 +0800

    check

--- src/app/domain/welcome/done/done.jsx
+++ src/app/domain/welcome/done/done.jsx
@@ -1,3 +1,40 @@
+import { useEffect, useState } from "react";
+import { useMainContext } from "../main.context";
+import axios from "axios";
+import { BASE_URL } from "../../../shared/config";
+
 export default function Done() {
-  return <div>Done</div>;
+  const { buyData, setBuyData, navigate } = useMainContext();
+  const [done, setDone] = useState(false);
+  const [dataSent, setDataSent] = useState(false);
+
+  const handleSendBuyData = async () => {
+    const res = await axios
+      .post(`${BASE_URL}/buy`, buyData)
+      .then((res) => {
+        if (res.status === 200) {
+          setDone(true);
+        }
+      })
+      .catch((err) => {
+        navigate(`/system/paper`);
+      });
+  };
+
+  useEffect(() => {
+    if (dataSent) {
+      return;
+    }
+    handleSendBuyData();
+    setDataSent(true);
+  });
+
+  useEffect(() => {
+    if (done) {
+      setBuyData({ paper: "", quantity: 0 });
+      navigate(`/system/paper`);
+    }
+  }, [done]);
+
+  return <div>{!done && <div>Please wait</div>}</div>;
 }
--- src/app/domain/welcome/start/start.jsx
+++ src/app/domain/welcome/start/start.jsx
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react";
 import FormattedCurrency from "../../../shared/components/formatted-currency";
 import axios from "axios";
 import { useMainContext } from "../main.context";
+import { Button } from "primereact/button";
+import { BASE_URL } from "../../../shared/config";
 
 export default function Start() {
-  const { buyData, navigate } = useMainContext();
+  const { buyData, setBuyData, navigate } = useMainContext();
   const [amount, setAmount] = useState(0);
   const handleGetCoinCount = async () => {
     const res = await axios
-      .get(`http://localhost:8000/coins`)
+      .get(`${BASE_URL}/coins`)
       .then((res) => {
         if (res.status === 200) {
           setAmount(res.data?.coins);
@@ -20,24 +22,34 @@ export default function Start() {
   };
 
   useEffect(() => {
-    setInterval(handleGetCoinCount, 1000);
+    const intervalId = setInterval(handleGetCoinCount, 1000);
+
+    return () => {
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
+    };
   }, []);
 
-  useEffect(() => {
-    if (amount >= buyData.quantity);
-    {
-      console.log(`done`);
-    }
-  }, [amount]);
   return (
-    <div className="flex flex-1 space-y-10 flex-col items-center h-full">
+    <div className="flex flex-1 space-y-5 flex-col items-center h-full">
       <div>
-        <h1 className="text-3xl">Start Transaction</h1>
+        <h1 className="text-2xl">Insert Coins</h1>
       </div>
       <div>
         <h1 className="text-[8em] font-semibold">
           <FormattedCurrency amount={amount} />
         </h1>
+      </div>
+      <div>
+        <Button
+          onClick={() => {
+            navigate(`/system/done`);
+          }}
+          className="bg-blue-600 text-white font-bold px-5 py-5 text-4xl rounded-md"
+        >
+          Done
+        </Button>
       </div>
     </div>
   );
